30cf58cb1ab26d7a79ef853c84af4e9fbc65fee5
dr.allgood <dr.allgood@brendamour.de>
2014-02-05 11:59:35 +0100

    it's 2014

--- jpasskit/src/main/java/de/brendamour/jpasskit/enums/PKBarcodeFormat.java
+++ jpasskit/src/main/java/de/brendamour/jpasskit/enums/PKBarcodeFormat.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2012 Patrice Brend'amour <p.brendamour@bitzeche.de>
+ * Copyright (C) 2014 Patrice Brend'amour <p.brendamour@bitzeche.de>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
