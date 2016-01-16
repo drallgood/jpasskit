c69cfd67a46b61f879869864bbe9299e0a384dae
Igor Stepanov <igor.stepanov@keystonett.com>
2016-01-16 11:41:04 +0200

    Update tests, get rid of code duplication https://github.com/stepio/jpasskit/issues/16

--- jpasskit/src/test/java/de/brendamour/jpasskit/signing/PKFileBasedSigningUtilTest.java
+++ jpasskit/src/test/java/de/brendamour/jpasskit/signing/PKFileBasedSigningUtilTest.java
@@ -15,28 +15,22 @@
  */
 package de.brendamour.jpasskit.signing;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.charset.Charset;
-import java.nio.file.Paths;
-import java.security.Security;
-import java.util.Arrays;
-import java.util.Date;
-
-import org.apache.commons.io.IOUtils;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
-
 import de.brendamour.jpasskit.PKBarcode;
 import de.brendamour.jpasskit.PKPass;
 import de.brendamour.jpasskit.enums.PKBarcodeFormat;
+import org.apache.commons.io.IOUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.charset.Charset;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Date;
 
 public class PKFileBasedSigningUtilTest {
 
@@ -46,9 +40,8 @@ public class PKFileBasedSigningUtilTest {
     private static final String keyStorePassword = "password";
 
     @Test
-    public void testManifest() throws IOException, Exception {
+    public void testManifest() throws Exception {
 
-        Security.addProvider(new BouncyCastleProvider());
         File temporaryPassDir = new File("target/");
         File manifestJSONFile = new File(getPathFromClasspath("pass2.json"));
 
@@ -59,37 +52,17 @@ public class PKFileBasedSigningUtilTest {
     }
 
     @Test
-    public void testPassZipGeneration() throws IOException, Exception {
-
-        Security.addProvider(new BouncyCastleProvider());
-        IPKSigningUtil pkSigningUtil = new PKFileBasedSigningUtil();
-
-        ObjectMapper jsonObjectMapper = new ObjectMapper();
-        PKPass pass = jsonObjectMapper.readValue(new File(getPathFromClasspath("pass2.json")), PKPass.class);
+    public void testFileBasedSigningWithLoadedPass() throws Exception {
+        PKPass pass = new ObjectMapper().readValue(new File(getPathFromClasspath("pass2.json")), PKPass.class);
         pass.setRelevantDate(new Date());
-        pass.getBarcodes().get(0).setMessageEncoding(Charset.forName("utf-8"));
+        pass.getBarcode().setMessageEncoding(Charset.forName("utf-8"));
         pass.setUserInfo(ImmutableMap.<String, Object>of("name", "John Doe"));
-        
-        PKSigningInformation pkSigningInformation = new PKSigningInformationUtil().loadSigningInformationFromPKCS12AndIntermediateCertificate(
-                keyStorePath, keyStorePassword, appleWWDRCA);
-        PKPassTemplateFolder passTemplate = new PKPassTemplateFolder(getPassFolderPath());
-        byte[] signedAndZippedPkPassArchive = pkSigningUtil.createSignedAndZippedPkPassArchive(pass, passTemplate, pkSigningInformation);
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(signedAndZippedPkPassArchive);
-        
-        File passZipFile = new File("target/passZIP.zip");
-        if (passZipFile.exists()) {
-            passZipFile.delete();
-        }
-        IOUtils.copy(inputStream, new FileOutputStream(passZipFile));
-        Assert.assertTrue(passZipFile.exists());
-        Assert.assertTrue(passZipFile.length() > 0);
-        AssertZip.assertValid(passZipFile);
+
+        createZipAndAssert(pass, "target/passFileBasedLoaded.zip");
     }
 
     @Test
-    public void testJson() throws IOException, Exception {
-        Security.addProvider(new BouncyCastleProvider());
-
+    public void testFileBasedSigningWithGeneratedPass() throws Exception {
         PKBarcode barcode = new PKBarcode();
         barcode.setFormat(PKBarcodeFormat.PKBarcodeFormatQR);
         barcode.setMessage("abcdefg");
@@ -100,13 +73,18 @@ public class PKFileBasedSigningUtilTest {
         pass.setPassTypeIdentifier("pti");
         pass.setTeamIdentifier("ti");
 
-        PKSigningInformation pkSigningInformation = new PKSigningInformationUtil().loadSigningInformationFromPKCS12AndIntermediateCertificate(
-                keyStorePath, keyStorePassword, appleWWDRCA);
-        byte[] signedAndZippedPkPassArchive = new PKFileBasedSigningUtil().createSignedAndZippedPkPassArchive(pass,
-                new PKPassTemplateFolder(getPassFolderPath()), pkSigningInformation);
+        createZipAndAssert(pass, "target/passFileBasedGenerated.zip");
+    }
+
+    private void createZipAndAssert(PKPass pkPass, String fileName) throws Exception {
+        PKSigningInformation pkSigningInformation = new PKSigningInformationUtil()
+                .loadSigningInformationFromPKCS12AndIntermediateCertificate(keyStorePath, keyStorePassword, appleWWDRCA);
+        PKPassTemplateFolder pkPassTemplate = new PKPassTemplateFolder(getPathFromClasspath(PASS_TEMPLATE_FOLDER));
+        IPKSigningUtil pkSigningUtil = new PKFileBasedSigningUtil();
+        byte[] signedAndZippedPkPassArchive = pkSigningUtil.createSignedAndZippedPkPassArchive(pkPass, pkPassTemplate, pkSigningInformation);
         ByteArrayInputStream inputStream = new ByteArrayInputStream(signedAndZippedPkPassArchive);
-        
-        File passZipFile = new File("target/passJson.zip");
+
+        File passZipFile = new File(fileName);
         if (passZipFile.exists()) {
             passZipFile.delete();
         }
@@ -116,11 +94,7 @@ public class PKFileBasedSigningUtilTest {
         AssertZip.assertValid(passZipFile);
     }
 
-    private String getPassFolderPath() throws URISyntaxException {
-        return getPathFromClasspath(PASS_TEMPLATE_FOLDER);
-    }
-
-    private String getPathFromClasspath(String path) throws URISyntaxException {
+    private String getPathFromClasspath(String path) throws Exception {
         return Paths.get(ClassLoader.getSystemResource(path).toURI()).toString();
     }
 }
