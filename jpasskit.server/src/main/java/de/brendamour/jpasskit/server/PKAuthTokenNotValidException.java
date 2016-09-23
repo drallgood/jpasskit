c19c0252d28d14213241de8c01ff90186a729ac2
Patrice Brend'amour <patrice@brendamour.net>
2016-09-23 21:57:20 +0200

    updated license header

--- jpasskit.server/src/main/java/de/brendamour/jpasskit/server/PKAuthTokenNotValidException.java
+++ jpasskit.server/src/main/java/de/brendamour/jpasskit/server/PKAuthTokenNotValidException.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2015 Patrice Brend'amour <patrice@brendamour.net>
+ * Copyright (C) 2016 Patrice Brend'amour <patrice@brendamour.net>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package de.brendamour.jpasskit.server;
 
 public class PKAuthTokenNotValidException extends Exception {
